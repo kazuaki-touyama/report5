0322dfdb3ff52a7077a0901a93ac11c78f4efee0
當山一朗 <touyamakazuaki@touyamaichirounoMacBook-Air.local>
2023-02-04 19:17:13 +0900

    report5

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e225754/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e225754;
+
+public class Main{
+    public static void main(String[] args){
+        String str;
+        int value;
+
+        str = "壱百満";
+        value = Integer.parseInt(str);
+    }
+}
